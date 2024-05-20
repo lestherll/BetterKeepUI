586d211e20725c30479a5b6025da6d8c02c8c4c2
verosh <68818846+verosh@users.noreply.github.com>
2024-05-20 15:14:24 +0100

    added simple baselayout (#1)
    * added baselayout
    
    * removed angular.json from gitignore

---
+++ src/app/base-layout/base-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-base-layout',
+  templateUrl: './base-layout.component.html',
+  styleUrl: './base-layout.component.css'
+})
+export class BaseLayoutComponent {
+
+}
