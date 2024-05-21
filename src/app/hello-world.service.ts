4e9e619c9a5f08f4ddba9ab408b49b80920083c6
WendellWen6 <wenzhonghao0@gmail.com>
2024-05-21 17:16:36 +0100

    init hello world service with restapi

--- src/app/hello-world.service.ts
+++ src/app/hello-world.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelloWorldService {
+  private helloWorldUrl = 'https://localhost:8080/hello';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  getHelloWorldMessage(): Observable<any> {
+    return this.http.get(this.helloWorldUrl);
+  }
 }
